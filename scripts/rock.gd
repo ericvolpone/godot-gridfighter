4a52e6d44fb099a1933dc84c433faa6659593f9f
Eric Volpone <ericvolpone@gmail.com>
2025-07-18 11:58:27 -0700

    Fixing rock deletion

--- scripts/rock.gd
+++ scripts/rock.gd
@@ -3,5 +3,6 @@ extends Node3D
 @onready var rigid_body: RigidBody3D = $RigidBody3D;
 
 func _physics_process(delta: float) -> void:
-	if(global_position.y < -5.0):
+	if(rigid_body.global_position.y < -5.0):
+		print("Fell off map, deleting rock");
 		queue_free();
