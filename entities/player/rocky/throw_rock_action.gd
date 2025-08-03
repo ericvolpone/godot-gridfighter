c2db941f69d5be360c672b32d8d70d2954e27a6a
Eric Volpone <ericvolpone@gmail.com>
2025-08-03 14:37:34 -0700

    Working on projectile spawner

--- entities/player/rocky/throw_rock_action.gd
+++ entities/player/rocky/throw_rock_action.gd
@@ -5,17 +5,18 @@ const rock_scene: PackedScene = preload("res://entities/objects/combat/rock.tscn
 func get_cd_time() -> float:
 	return 3.0;
 
-@rpc("call_local", "reliable")
 func execute_child() -> void:
 	# Spawn a rock
 	var rock: RigidBody3D = rock_scene.instantiate();
 	var player: Player = get_player();
 	var lobby: Node3D = get_lobby();
+	rock.set_multiplayer_authority(multiplayer.get_unique_id())
 	
 	lobby.add_child(rock);
 	rock.global_position = global_position + (player.mesh.get_global_transform().basis.z.normalized());
 	rock.global_position.y += 1;
 	rock.apply_impulse(player.mesh.get_global_transform().basis.z * 50)
+	print("ROCK READY on peer:", multiplayer.get_unique_id(), " | authority:", get_multiplayer_authority(), " | is_authority:", is_multiplayer_authority())
 
 func is_usable_child() -> bool:
 	return true;
